e82df05100f7a0c22566c041abbf82a84792ac6c
VirtualTam <virtualtam@flibidi.net>
2022-03-03 00:43:54 +0100

    decoder: introduce Decoder type to group methods
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- decoder/decoder.go
+++ decoder/decoder.go
@@ -1,11 +1,136 @@
 package decoder
 
 import (
+	"net/url"
+	"sort"
+	"strings"
+
 	"github.com/virtualtam/netscape-go/ast"
 	"github.com/virtualtam/netscape-go/types"
 )
 
 // Decode walks a Netscape Bookmark AST and returns the corresponding document.
 func Decode(f ast.File) (types.Document, error) {
-	return decodeFile(f)
+	var d Decoder
+	return d.decodeFile(f)
+}
+
+type Decoder struct{}
+
+func (d *Decoder) decodeFile(f ast.File) (types.Document, error) {
+	document := types.Document{
+		Title: f.Title,
+	}
+
+	root, err := d.decodeFolder(f.Root)
+	if err != nil {
+		return types.Document{}, err
+	}
+	document.Root = root
+
+	return document, nil
+}
+
+func (d *Decoder) decodeFolder(f ast.Folder) (types.Folder, error) {
+	folder := types.Folder{
+		Name:        f.Name,
+		Description: f.Description,
+		Attributes:  map[string]string{},
+	}
+
+	for attr, value := range f.Attributes {
+		switch attr {
+		case createdAtAttr:
+			createdAt, err := decodeDate(value)
+			if err != nil {
+				return types.Folder{}, err
+			}
+			folder.CreatedAt = createdAt
+		case updatedAtAttr:
+			updatedAt, err := decodeDate(value)
+			if err != nil {
+				return types.Folder{}, err
+			}
+			folder.UpdatedAt = updatedAt
+		default:
+			folder.Attributes[attr] = value
+		}
+	}
+
+	for _, b := range f.Bookmarks {
+		bookmark, err := d.decodeBookmark(b)
+		if err != nil {
+			return types.Folder{}, err
+		}
+
+		folder.Bookmarks = append(folder.Bookmarks, bookmark)
+	}
+
+	for _, sf := range f.Subfolders {
+		subfolder, err := d.decodeFolder(sf)
+		if err != nil {
+			return types.Folder{}, err
+		}
+
+		folder.Subfolders = append(folder.Subfolders, subfolder)
+	}
+
+	return folder, nil
+}
+
+func (d Decoder) decodeBookmark(b ast.Bookmark) (types.Bookmark, error) {
+	bookmark := types.Bookmark{
+		Title:       b.Title,
+		Description: b.Description,
+		Attributes:  map[string]string{},
+	}
+
+	url, err := url.Parse(b.Href)
+	if err != nil {
+		return types.Bookmark{}, err
+	}
+	bookmark.URL = *url
+
+	for attr, value := range b.Attributes {
+		switch attr {
+		case createdAtAttr:
+			createdAt, err := decodeDate(value)
+			if err != nil {
+				return types.Bookmark{}, err
+			}
+			bookmark.CreatedAt = createdAt
+		case updatedAtAttr:
+			updatedAt, err := decodeDate(value)
+			if err != nil {
+				return types.Bookmark{}, err
+			}
+			bookmark.UpdatedAt = updatedAt
+		case privateAttr:
+			if value == "1" {
+				bookmark.Private = true
+			}
+		case tagsAttr:
+			bookmark.Tags = d.decodeTags(b.Attributes)
+		default:
+			bookmark.Attributes[attr] = value
+		}
+	}
+
+	return bookmark, nil
+}
+
+func (d *Decoder) decodeTags(attr map[string]string) []string {
+	rawTags, ok := attr[tagsAttr]
+	if !ok {
+		return []string{}
+	}
+
+	tags := strings.Split(rawTags, ",")
+	for index, tag := range tags {
+		tags[index] = strings.TrimSpace(tag)
+	}
+
+	sort.Strings(tags)
+
+	return tags
 }
---
+++ decoder/decoder_test.go
@@ -0,0 +1,471 @@
+package decoder
+
+import (
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/virtualtam/netscape-go/ast"
+	"github.com/virtualtam/netscape-go/types"
+)
+
+func TestDecodeFile(t *testing.T) {
+	cases := []struct {
+		tname string
+		file  ast.File
+		want  types.Document
+	}{
+		{
+			tname: "empty document",
+		},
+		{
+			tname: "flat document",
+			file: ast.File{
+				Title: "Bookmarks",
+				Root: ast.Folder{
+					Name:        "Test Folder",
+					Description: "Add bookmarks to this folder",
+					Bookmarks: []ast.Bookmark{
+						{
+							Href:  "https://domain.tld",
+							Title: "Test Domain",
+						},
+						{
+							Description: "Second test",
+							Href:        "https://test.domain.tld",
+							Title:       "Test Domain II",
+						},
+					},
+				},
+			},
+			want: types.Document{
+				Title: "Bookmarks",
+				Root: types.Folder{
+					Name:        "Test Folder",
+					Description: "Add bookmarks to this folder",
+					Bookmarks: []types.Bookmark{
+						{
+							URL: url.URL{
+								Scheme: "https",
+								Host:   "domain.tld",
+							},
+							Title: "Test Domain",
+						},
+						{
+							Description: "Second test",
+							URL: url.URL{
+								Scheme: "https",
+								Host:   "test.domain.tld",
+							},
+							Title: "Test Domain II",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.tname, func(t *testing.T) {
+			var d Decoder
+			got, err := d.decodeFile(tc.file)
+
+			if err != nil {
+				t.Errorf("expected no error, got %q", err)
+			}
+
+			if got.Title != tc.want.Title {
+				t.Errorf("want title %q, got %q", tc.want.Title, got.Title)
+			}
+
+			assertFoldersEqual(t, got.Root, tc.want.Root)
+		})
+	}
+}
+
+func TestDecodeFolder(t *testing.T) {
+	cases := []struct {
+		tname string
+		input ast.Folder
+		want  types.Folder
+	}{
+		{
+			tname: "empty folder",
+			input: ast.Folder{
+				Name:        "Test Folder",
+				Description: "Add bookmarks to this folder",
+			},
+			want: types.Folder{
+				Name:        "Test Folder",
+				Description: "Add bookmarks to this folder",
+			},
+		},
+		{
+			tname: "empty folder with creation and update dates, and extra attributes",
+			input: ast.Folder{
+				Name:        "Test Folder",
+				Description: "Add bookmarks to this folder",
+				Attributes: map[string]string{
+					"ADD_DATE":                "1646154673",
+					"LAST_MODIFIED":           "1646172586",
+					"PERSONAL_TOOLBAR_FOLDER": "true",
+				},
+			},
+			want: types.Folder{
+				CreatedAt:   time.Date(2022, time.March, 1, 17, 11, 13, 0, time.UTC),
+				UpdatedAt:   time.Date(2022, time.March, 1, 22, 9, 46, 0, time.UTC),
+				Name:        "Test Folder",
+				Description: "Add bookmarks to this folder",
+				Attributes: map[string]string{
+					"PERSONAL_TOOLBAR_FOLDER": "true",
+				},
+			},
+		},
+		{
+			tname: "folder with bookmarks",
+			input: ast.Folder{
+				Name:        "Test Folder",
+				Description: "Add bookmarks to this folder",
+				Bookmarks: []ast.Bookmark{
+					{
+						Href:  "https://domain.tld",
+						Title: "Test Domain",
+					},
+					{
+						Description: "Second test",
+						Href:        "https://test.domain.tld",
+						Title:       "Test Domain II",
+					},
+				},
+			},
+			want: types.Folder{
+				Name:        "Test Folder",
+				Description: "Add bookmarks to this folder",
+				Bookmarks: []types.Bookmark{
+					{
+						URL: url.URL{
+							Scheme: "https",
+							Host:   "domain.tld",
+						},
+						Title: "Test Domain",
+					},
+					{
+						Description: "Second test",
+						URL: url.URL{
+							Scheme: "https",
+							Host:   "test.domain.tld",
+						},
+						Title: "Test Domain II",
+					},
+				},
+			},
+		},
+		{
+			tname: "folder with sub-folders and bookmarks",
+			input: ast.Folder{
+				Name:        "Bookmarks",
+				Description: "Root Folder",
+				Bookmarks: []ast.Bookmark{
+					{
+						Href:  "https://domain.tld",
+						Title: "Test Domain",
+					},
+					{
+						Description: "Second test",
+						Href:        "https://test.domain.tld",
+						Title:       "Test Domain II",
+					},
+				},
+				Subfolders: []ast.Folder{
+					{
+						Name: "Empty",
+					},
+					{
+						Name: "Personal Toolbar",
+						Bookmarks: []ast.Bookmark{
+							{
+								Href:  "https://personal.tld",
+								Title: "Personal Domain",
+							},
+							{
+								Description: "Weather Reports",
+								Href:        "https://weather.tld",
+								Title:       "Weather Reports",
+							},
+						},
+					},
+				},
+			},
+			want: types.Folder{
+				Name:        "Bookmarks",
+				Description: "Root Folder",
+				Bookmarks: []types.Bookmark{
+					{
+						URL: url.URL{
+							Scheme: "https",
+							Host:   "domain.tld",
+						},
+						Title: "Test Domain",
+					},
+					{
+						Description: "Second test",
+						URL: url.URL{
+							Scheme: "https",
+							Host:   "test.domain.tld",
+						},
+						Title: "Test Domain II",
+					},
+				},
+				Subfolders: []types.Folder{
+					{
+						Name: "Empty",
+					},
+					{
+						Name: "Personal Toolbar",
+						Bookmarks: []types.Bookmark{
+							{
+								URL: url.URL{
+									Scheme: "https",
+									Host:   "personal.tld",
+								},
+								Title: "Personal Domain",
+							},
+							{
+								Description: "Weather Reports",
+								URL: url.URL{
+									Scheme: "https",
+									Host:   "weather.tld",
+								},
+								Title: "Weather Reports",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.tname, func(t *testing.T) {
+			var d Decoder
+			got, err := d.decodeFolder(tc.input)
+
+			if err != nil {
+				t.Fatalf("expected no error, got %q", err)
+			}
+
+			assertFoldersEqual(t, got, tc.want)
+		})
+	}
+}
+
+func assertFoldersEqual(t *testing.T, got types.Folder, want types.Folder) {
+	t.Helper()
+
+	if got.CreatedAt != want.CreatedAt {
+		t.Errorf("want creation date %q, got %q", want.CreatedAt.String(), got.CreatedAt.String())
+	}
+
+	if got.UpdatedAt != want.UpdatedAt {
+		t.Errorf("want update date %q, got %q", want.UpdatedAt.String(), got.UpdatedAt.String())
+	}
+
+	if got.Description != want.Description {
+		t.Errorf("want description %q, got %q", want.Description, got.Description)
+	}
+
+	if got.Name != want.Name {
+		t.Errorf("want name %q, got %q", want.Name, got.Name)
+	}
+
+	assertAttributesEqual(t, got.Attributes, want.Attributes)
+
+	if len(got.Bookmarks) != len(want.Bookmarks) {
+		t.Fatalf("want %d bookmarks, got %d", len(want.Bookmarks), len(got.Bookmarks))
+	}
+
+	for index, wantBookmark := range want.Bookmarks {
+		assertBookmarksEqual(t, got.Bookmarks[index], wantBookmark)
+	}
+
+	if len(got.Subfolders) != len(want.Subfolders) {
+		t.Fatalf("want %d subfolders, got %d", len(want.Subfolders), len(got.Subfolders))
+	}
+
+	for index, wantSubfolder := range want.Subfolders {
+		assertFoldersEqual(t, got.Subfolders[index], wantSubfolder)
+	}
+}
+func TestDecodeBookmark(t *testing.T) {
+	cases := []struct {
+		tname string
+		input ast.Bookmark
+		want  types.Bookmark
+	}{
+		{
+			tname: "bookmark with mandatory information only",
+			input: ast.Bookmark{
+				Href:  "https://domain.tld",
+				Title: "Test Domain",
+			},
+			want: types.Bookmark{
+				Title: "Test Domain",
+				URL: url.URL{
+					Scheme: "https",
+					Host:   "domain.tld",
+				},
+			},
+		},
+		{
+			tname: "bookmark with multi-line description",
+			input: ast.Bookmark{
+				Description: "Nested lists:\n- list1\n  - item1.1\n  - item1.2\n  - item1.3\n- list2\n  - item2.1",
+				Href:        "https://domain.tld",
+				Title:       "Test Domain",
+			},
+			want: types.Bookmark{
+				Title: "Test Domain",
+				URL: url.URL{
+					Scheme: "https",
+					Host:   "domain.tld",
+				},
+				Description: "Nested lists:\n- list1\n  - item1.1\n  - item1.2\n  - item1.3\n- list2\n  - item2.1",
+			},
+		},
+		{
+			tname: "bookmark with creation and update date",
+			input: ast.Bookmark{
+				Href:  "https://domain.tld",
+				Title: "Test Domain",
+				Attributes: map[string]string{
+					"ADD_DATE":      "1646154673",
+					"LAST_MODIFIED": "1646172586",
+				},
+			},
+			want: types.Bookmark{
+				CreatedAt: time.Date(2022, time.March, 1, 17, 11, 13, 0, time.UTC),
+				UpdatedAt: time.Date(2022, time.March, 1, 22, 9, 46, 0, time.UTC),
+				Title:     "Test Domain",
+				URL: url.URL{
+					Scheme: "https",
+					Host:   "domain.tld",
+				},
+			},
+		},
+		{
+			tname: "private bookmark",
+			input: ast.Bookmark{
+				Href:  "https://domain.tld",
+				Title: "Test Domain",
+				Attributes: map[string]string{
+					"PRIVATE": "1",
+				},
+			},
+			want: types.Bookmark{
+				Title: "Test Domain",
+				URL: url.URL{
+					Scheme: "https",
+					Host:   "domain.tld",
+				},
+				Private: true,
+			},
+		},
+		{
+			tname: "bookmark with comma-separated tags and extra whitespace",
+			input: ast.Bookmark{
+				Href:  "https://domain.tld",
+				Title: "Test Domain",
+				Attributes: map[string]string{
+					"TAGS": "test, netscape,     bookmark",
+				},
+			},
+			want: types.Bookmark{
+				Title: "Test Domain",
+				URL: url.URL{
+					Scheme: "https",
+					Host:   "domain.tld",
+				},
+				Tags: []string{
+					"bookmark",
+					"netscape",
+					"test",
+				},
+			},
+		},
+		{
+			tname: "bookmark with extra attributes",
+			input: ast.Bookmark{
+				Href:  "https://domain.tld",
+				Title: "Test Domain",
+				Attributes: map[string]string{
+					"ICON_URI":     "https://domain.tld/favicon.ico",
+					"LAST_CHARSET": "windows-1252",
+					"PRIVATE":      "1",
+				},
+			},
+			want: types.Bookmark{
+				Title: "Test Domain",
+				URL: url.URL{
+					Scheme: "https",
+					Host:   "domain.tld",
+				},
+				Private: true,
+				Attributes: map[string]string{
+					"ICON_URI":     "https://domain.tld/favicon.ico",
+					"LAST_CHARSET": "windows-1252",
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.tname, func(t *testing.T) {
+			var d Decoder
+			got, err := d.decodeBookmark(tc.input)
+
+			if err != nil {
+				t.Errorf("expected no error, got %q", err)
+			}
+
+			assertBookmarksEqual(t, got, tc.want)
+		})
+	}
+}
+
+func assertBookmarksEqual(t *testing.T, got types.Bookmark, want types.Bookmark) {
+	if got.CreatedAt != want.CreatedAt {
+		t.Errorf("want creation date %q, got %q", want.CreatedAt.String(), got.CreatedAt.String())
+	}
+
+	if got.UpdatedAt != want.UpdatedAt {
+		t.Errorf("want update date %q, got %q", want.UpdatedAt.String(), got.UpdatedAt.String())
+	}
+
+	if got.Title != want.Title {
+		t.Errorf("want title %q, got %q", want.Title, got.Title)
+	}
+
+	if got.URL != want.URL {
+		t.Errorf("want URL %q, got %q", want.URL.String(), got.URL.String())
+	}
+
+	if got.Description != want.Description {
+		t.Errorf("want description %q, got %q", want.Description, got.Description)
+	}
+
+	if got.Private != want.Private {
+		t.Errorf("want private %t, got %t", want.Private, got.Private)
+	}
+
+	if len(got.Tags) != len(want.Tags) {
+		t.Fatalf("want %d tags, got %d", len(want.Tags), len(got.Tags))
+	}
+
+	for index, wantTag := range want.Tags {
+		if got.Tags[index] != wantTag {
+			t.Errorf("want tag %d value %q, got %q", index, wantTag, got.Tags[index])
+		}
+	}
+
+	assertAttributesEqual(t, got.Attributes, want.Attributes)
+}
