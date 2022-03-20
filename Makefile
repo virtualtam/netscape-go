b53ac031fea1a6a8ba4fb252c4028232dab8e7f9
VirtualTam <virtualtam@flibidi.net>
2022-03-20 23:55:46 +0100

    netscape-go: add benchmarks
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- Makefile
+++ Makefile
@@ -1,15 +1,40 @@
+BENCH_DIR ?= bench
 BUILD_DIR ?= build
+SRC_FILES := $(shell find . -name "*.go")
 
 all: lint cover build
 .PHONY: all
 
+bench: $(BENCH_DIR)/Marshal.txt $(BENCH_DIR)/Unmarshal.txt
+.PHONY: bench
+
+$(BENCH_DIR)/%.txt: $(SRC_FILES)
+	@mkdir -p $(BENCH_DIR)
+	go test \
+		-bench=Benchmark$* \
+		-run=^$$ \
+		-benchmem \
+		-cpuprofile=$(BENCH_DIR)/$*.cpuprof \
+		-memprofile=$(BENCH_DIR)/$*.memprof \
+		-trace=$(BENCH_DIR)/$*.trace \
+	| tee $@
+	@echo
+	benchstat $@
+	@echo
+	@echo "View benchmark profiles with:"
+	@echo "  go tool pprof -http=:8081 $(BENCH_DIR)/$*.cpuprof"
+	@echo "  go tool pprof -http=:8082 $(BENCH_DIR)/$*.memprof"
+
+build: \
+	$(BUILD_DIR)/roundtrip \
+	$(BUILD_DIR)/unmarshal
+
+$(BUILD_DIR)/%: $(SRC_FILES)
+	go build -trimpath -o $@ ./cmd/$*
+
 lint:
 	golangci-lint run ./...
 .PHONY: lint
-
-test:
-	go test ./...
-.PHONY: test
 
 cover:
 	go test -coverprofile=coverage.out ./...
@@ -19,9 +44,6 @@ coverhtml: cover
 	go tool cover -html=coverage.out
 .PHONY: coverhtml
 
-build: \
-	$(BUILD_DIR)/roundtrip \
-	$(BUILD_DIR)/unmarshal
-
-$(BUILD_DIR)/%: $(shell find . -type f -name "*.go")
-	go build -trimpath -o $@ ./cmd/$*
+test:
+	go test ./...
+.PHONY: test
---
+++ marshal_benchmark_test.go
@@ -0,0 +1,77 @@
+package netscape_test
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/virtualtam/netscape-go"
+)
+
+var (
+	cases = []struct {
+		name     string
+		filepath string
+	}{
+		{
+			name:     "flat: 100 bookmarks",
+			filepath: "testdata/flat100.htm",
+		},
+		{
+			name:     "flat: 1000 bookmarks",
+			filepath: "testdata/flat1000.htm",
+		},
+		{
+			name:     "flat: 10000 bookmarks",
+			filepath: "testdata/flat10000.htm",
+		},
+	}
+)
+
+func BenchmarkMarshal(b *testing.B) {
+	b.ReportAllocs()
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			bytes, err := ioutil.ReadFile(tc.filepath)
+			if err != nil {
+				b.Fatalf("failed to open file %q: %s", tc.filepath, err)
+			}
+
+			document, err := netscape.Unmarshal(bytes)
+			if err != nil {
+				b.Fatalf("failed to open file %q: %s", tc.filepath, err)
+			}
+
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				_, err := netscape.Marshal(document)
+				if err != nil {
+					b.Fatalf("failed to marshal document: %s", err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkUnmarshal(b *testing.B) {
+	b.ReportAllocs()
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			bytes, err := ioutil.ReadFile(tc.filepath)
+			if err != nil {
+				b.Fatalf("failed to open file %q: %s", tc.filepath, err)
+			}
+
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				_, err := netscape.Unmarshal(bytes)
+				if err != nil {
+					b.Fatalf("failed to open file %q: %s", tc.filepath, err)
+				}
+			}
+		})
+	}
+}
